d63e09f138b0769171aab7877a1a4b9f71e39587
William Le <williamkle92@gmail.com>
2025-04-19 22:33:16 -0700

    feat: add db connection

---
+++ database/db.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+	"fmt"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	db_name := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := CreateDSN(host, user, password, db_name, port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("❌ Failed to connect to database: ", err)
+	}
+
+	DB = db
+	log.Println("✅ Database connected")
+}
+
+
+
+func CreateDSN(host, user, password, dbName, port string) string {
+	return fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbName, port,
+	)
+}
