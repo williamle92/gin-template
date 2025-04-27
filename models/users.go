18c6ce0f33a2f5e9d4b876c3598f30435dd55c09
William Le <williamkle92@gmail.com>
2025-04-26 22:14:44 -0700

    feat: model changes

--- models/users.go
+++ models/users.go
@@ -6,10 +6,10 @@ import (
 
 type User struct {
 	gorm.Model
-	ID          uint    `gorm:"index" json:"id"`
-	FirstName   string  `json:"first_name"`
-	LastName    string  `json:"last_name"`
-	Email       string  `gorm:"unique" json:"email"`
-	PhoneNumber *string `json:"phone_number"`
-	Password    string  `json:"password"`
+	ID          uint   `gorm:"primaryKey;autoIncrement;index" json:"id"`
+	FirstName   string `json:"first_name" gorm:"not null"`
+	LastName    string `json:"last_name" gorm:"not null"`
+	Email       string `gorm:"unique;index;not null" json:"email"`
+	PhoneNumber string `gorm:"unique;index;not null" json:"phone_number"`
+	Password    string `json:"-"`
 }
